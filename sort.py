0b582382856445666fc362be96832fdeca50ba1c
BeardedIT <44595661+BeardedIT@users.noreply.github.com>
2019-05-01 22:29:14 -0600

    Cleaned up variable names

--- sort.py
+++ sort.py
@@ -1,33 +1,37 @@
+#! Python 3.7.3
 ## Todo
 ## -------------------------------------------------------
 ## Ensure that files that dont match are skipped
 ## Error Handling
 ##
+
+# Script requires filename to be in format
+#(Show) - (Season#)x(Episode#) - (Episode Title)
+# Built using python 3.7.3
 import os, shutil
-path = os.getcwd()
-names = os.listdir(path)
+base_path = os.getcwd()
+file_names = os.listdir(path)
 increment=0
 def ParseName(unsplit):
     try:
-        split_Name=unsplit.split(' - ')
+        parsed_Name=unsplit.split(' - ')
         unsplit=unsplit.replace(' ','')
-        season_and_episode=split_Name[1].split('x')
+        season_and_episode=parsed_Name[1].split('x')
         season=season_and_episode[0]
-        split_Name[1]='S'+season
-        return split_Name
+        parsed_Name[1]='S'+season
+        return parsed_Name
     except:
         print ('Sorry'+unsplit+'is not a valid file')
 for files in names:
     if files.endswith(".mp4") or files.endswith(".m4v") or files.endswith('.mkv')or files.endswith('.avi'):
         try:
             filename=ParseName(files)
-            dest_dir= os.path.join(path,str(filename[0]),str(filename[1]))
-            print (dest_dir)
+            dest_dir= os.path.join(base_path,str(filename[0]),str(filename[1]))
             if not os.path.exists(dest_dir):
                     os.makedirs(dest_dir)
-            shutil.move(path+os.sep+files,dest_dir)
-            print(path+os.sep+files + '-->' + dest_dir)
-            increment=increment+1
+            shutil.move(base_path+os.sep+files,dest_dir)
+            print(base_path+os.sep+files + '-->' + dest_dir)
+            increment+=1
         except:
             print('Sorry an error occurred')
 print(increment,'Files were moved. Have a nice day')
